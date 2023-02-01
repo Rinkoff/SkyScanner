0bd54ec14b9f9d1243ceac99e3a4c2b1452feecb
Rinkoff <leserbt@gmail.com>
2023-02-01 22:33:57 +0200

    Implement a db create and remove table query

--- Crawler/DB.py
+++ Crawler/DB.py
@@ -1,6 +1,47 @@
-import mysql.connector
+import mysql.connector as connector
+
+from read_db_config import read_db_config
+
 
 
 class DB():
     def __init__(self):
-        pass
+        # Read config.ini file and take database information
+        mysql_config=read_db_config("../config.ini","MySQL")
+
+        # Check that database is connected
+        try:
+            self.connector=connector.connect(mysql_config)
+
+        except connector.Error as error:
+            print(error)
+
+
+    def create_table(self):
+        # Query for create new table in database
+        mysql="""
+            CREATE TABLE IF NOT EXISTS Scanner(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            Country VARCHAR(100) NOT NULL,
+            City VARCHAR(100) NOT NULL,
+            Price VARCHAR(10) NOT NULL,
+            );
+        """
+
+        # Establish a cursor to interact with database
+        with self.connector.cursor() as cursor:
+            # Execute query in mysql variable and commit it
+            cursor.execute(mysql)
+            self.connector.commit()
+
+    def remove_table(self):
+        # Query for remove table in database
+        mysql="DROP TABLE IF EXISTS Scanner;"
+
+        # Establish a cursor to interact with database
+        with self.connector.cursor() as cursor:
+            # Execute query in mysql variable and commit it
+            cursor.execute(mysql)
+            self.connector.commit()
+
+
