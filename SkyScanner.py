6d5f4563af50565d4cfb5c5efe80cdab6de85f69
Rinkoff <leserbt@gmail.com>
2023-02-13 00:23:37 +0200

    Final app

--- SkyScanner.py
+++ SkyScanner.py
@@ -1,33 +1,69 @@
-#Import Packages
+# Import Packages
 import sys
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
 
-#Import Modules
+# Import Modules
 from UI.Main_Window_UI import Ui_Form
+from Crawler.crawler import Crawler
+from Crawler.DB import DB
+from UI.Table_view import Table_view
 
 
-#URL of the webpage that we are scraping information from
-BASE_URL="https://www.esky.bg/oferti/co/bg/0/0/balgariya"
+# URL of the webpage that we are scraping information from
+BASE_URL = "https://www.esky.bg/oferti/co/bg/0/0/balgariya"
 
-class Main_Window(qtw.QMainWindow,Ui_Form):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+
+class Main_Window(qtw.QMainWindow, Ui_Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         Ui_Form.__init__(self)
 
         self.setupUi(self)
 
+        # Set Title and Icon
         self.setWindowTitle("SkyScanner")
-        self.setWindowIcon(qtg.QIcon("./assets/Images/8bfa5d6a52a03e83b995fec69a4d8c2c.png"))
+        self.setWindowIcon(qtg.QIcon("./assets/Images/img.png"))
+
+        # Set fixed window size
+        self.setFixedSize(786, 654)
+
+        # Make click connections
+        self.btnRunCrawler.clicked.connect(self.onBtnRunCrawlerClick)
+        self.btnShowData.clicked.connect(self.onBtnShowDataClick)
 
         self.show()
 
+    @qtc.pyqtSlot(bool)
+    def onBtnRunCrawlerClick(self):
+        # Scrape through the website
+        crawler = Crawler(BASE_URL)
+        crawler.start()
+        crawler_info = crawler.info
+        crawler.stop()
+
+        # Delete old table and make new with new data
+        self.db = DB()
+        self.db.remove_table()
+        self.db.create_table()
+
+        # Insert information into db
+        self.db.intert_rows(offers=crawler_info)
+
+        # Open Table View
+        self.onBtnShowDataClick()
+        self.close()
+
+    @qtc.pyqtSlot(bool)
+    def onBtnShowDataClick(self):
+        self.table_view = Table_view()
+        self.table_view.show()
 
 
-if __name__ == '__main__':
-    app=qtw.QApplication(sys.argv)
+if __name__ == "__main__":
+    app = qtw.QApplication(sys.argv)
 
-    window=Main_Window()
+    window = Main_Window()
 
     sys.exit(app.exec())
